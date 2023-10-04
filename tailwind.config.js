dba68eac21687b519e39bfb2a5ab5971651b9bf5
Louis Van Aken <vanakenlouis@gmail.com>
2023-10-04 16:40:23 +0200

    Added services section

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,20 @@ export default {
       'white': 'rgb(255, 255, 255)',
       'white-80': 'rgba(255, 255, 255, .8)',
       'orange': 'rgb(255, 77, 0)',
+      'orange-dark': 'rgb(224, 65, 0)',
       'turquoise': 'rgb(26, 222, 210)',
       'pink': 'rgb(243, 101, 170)',
       'brown': 'rgb(67, 50, 42)',
       'brown-15': 'rgba(67, 50, 42, .15)',
       'brown-25': 'rgba(67, 50, 42, .30)',
       'light-brown': 'rgb(148, 134, 128)',
-      'light-grey': 'rgb(236, 236, 236)',
+      'light-grey': 'rgb(243, 243, 243)',
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'wave-pattern': "url('./assets/images/wave.svg')",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
